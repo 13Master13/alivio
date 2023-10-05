51f3891b10e42d6706fd8de3dcb9aa5198b302b4
13Master13 <141366261+13Master13@users.noreply.github.com>
2023-10-05 19:57:09 +0300

    Finish

--- js/main.js
+++ js/main.js
@@ -10,14 +10,26 @@ mobileNavBTN.addEventListener('click', function () {
 
 // VIDEO
 const videoBtn = document.querySelector('#video-story-btn')
-const videoBtnIcon = document.querySelector('#video-story-btn-icon')
+const videoBtnIcon = document.querySelector('#story-video-btn-icon')
+const videoOverlay = document.querySelector('#video-story-overlay')
 const videoFile = document.querySelector('#video-story')
 videoBtn.addEventListener('click', function () {
+	function toggleOverlay(event) {
+		if (event.type === 'mouseleave') {
+			videoOverlay.classList.add('hidden')
+		} else {
+			videoOverlay.classList.remove('hidden')
+		}
+	}
 	if (videoFile.paused) {
 		videoFile.play()
 		videoBtnIcon.src = './img/pause-white.svg'
+		videoOverlay.onmouseleave = toggleOverlay
+		videoOverlay.onmouseenter = toggleOverlay
 	} else {
 		videoFile.pause()
 		videoBtnIcon.src = './img/play-white.svg'
+		videoOverlay.onmouseleave = null
+		videoOverlay.onmouseenter = null
 	}
 })
